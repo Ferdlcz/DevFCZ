c6325eac078d86dd86b31022c552a718ebaac1e3
Ferdlcz <ferdlcskate@gmail.com>
2023-04-20 15:55:49 -0600

    V1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById('toggle-sidebar');
+const sidebar = document.querySelector('.sidebar');
+
+toggleButton.addEventListener('click', function() {
+  sidebar.classList.toggle('show');
+    
+});
